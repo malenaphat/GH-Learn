04fd259f588930ce2666437ac0de1ce532fd05a5
Naphat Nawamongkhonchaikit <malenaphat@gmail.com>
2021-04-30 11:40:38 +0700

    Initial Commit

---
+++ GH-Learn/GH_LearnApp.swift
@@ -0,0 +1,17 @@
+//
+//  GH_LearnApp.swift
+//  GH-Learn
+//
+//  Created by Naphat nawamongkhonchaikit on 30/4/2564 BE.
+//
+
+import SwiftUI
+
+@main
+struct GH_LearnApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
